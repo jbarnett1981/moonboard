4d4d7d3a292a2edb693420d29c9c8e998b7156e9
Ryan Palo <ryan@giantsource.com>
2020-05-02 14:57:11 -0700

    Specifies initial display for Chromium browser

--- scripts/kiosk_browser.sh
+++ scripts/kiosk_browser.sh
@@ -10,4 +10,4 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost
+DISPLAY=:0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost
